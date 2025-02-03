0744eca887117ad6bd5fa6d1cdeddd2cbdc3b464
Tudor Butufei <tudor.butufei@gmail.com>
2025-02-03 07:03:29 +0200

    Implement admin users page

--- app/admin/users/page.tsx
+++ app/admin/users/page.tsx
@@ -1,8 +1,33 @@
-export default function AdminUsersPage() {
+import { Badge } from "@/components/ui/badge";
+import { getAllUsers } from "@/lib/controllers/userController"
+import { notFound } from "next/navigation";
+
+export default async function AdminUsersPage() {
+  const users = await getAllUsers();
+  if (!users) {
+    return notFound();
+  }
+
   return (
     <>
       <h2 className="text-xl font-bold mb-2">Elevi</h2>
+      <ul>
+        {users.students.map(s => (
+          <li className="my-2 px-3 py-2 border rounded flex gap-2 items-center bg-white" key={`test-${s.id}`}>
+            <span className="mr-auto py-1">{s.firstname} {s.lastname}</span>
+            <Badge variant="secondary" className="bg-green-300 hover:bg-green-300">{s.creditPoints} puncte credit</Badge>
+          </li>
+        ))}
+      </ul>
       <h2 className="text-xl font-bold mb-2">Profesori</h2>
+      <ul>
+        {users.teachers.map(t => (
+          <li className="my-2 px-3 py-2 border rounded flex gap-2 items-center bg-white" key={`test-${t.id}`}>
+          <span className="mr-auto py-1">{t.firstname} {t.lastname}</span>
+          <Badge variant="secondary">{t.subject.name}</Badge>
+        </li>
+        ))}
+      </ul>
     </>
   )
 }
--- lib/controllers/userController.ts
+++ lib/controllers/userController.ts
@@ -8,7 +8,7 @@ export async function getCurrentUser() {
     return null;
   }
 
-  const {data: userData, error} = await supabase.from("Users")
+  const userQuery = await supabase.from("Users")
     .select(`
       *,
       teacher:Teachers(*),
@@ -17,9 +17,40 @@ export async function getCurrentUser() {
     .eq("email", user.data.user.email)
     .single();
 
-  if (error) {
-    console.log(error);
+  if (userQuery.error) {
+    console.log("Get current user", userQuery.error);
   }
 
-  return userData;
+  return userQuery.data;
+}
+
+export async function getAllUsers() {
+  const supabase = await createClient();
+
+  const teachersQuery = await supabase.from("Teachers")
+    .select(`
+      *,
+      subject:Subjects!inner(name),
+      createdTests:PracticeTests(id)
+    `, {
+      count:"exact"
+    });
+  console.log(teachersQuery.data);
+
+  const studentsQuery = await supabase.from("Students").select("*");
+
+  if (teachersQuery.error) {
+    console.log("Get all teachers", teachersQuery.error);
+    return null;
+  }
+
+  if (studentsQuery.error) {
+    console.log("Get all students", studentsQuery.error);
+    return null;
+  }
+
+  return {
+    students: studentsQuery.data,
+    teachers: teachersQuery.data
+  }
 }
